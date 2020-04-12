3fe1801e00d51b0b4d2f76c03d1749367128c335
Gu <gubw>
2020-04-13 01:12:29 +0800

    ver0.0.2 增加demo http://localhost:8080/demo/test restful风格api

--- gs-demo/src/main/java/GsApplication.java
+++ gs-demo/src/main/java/GsApplication.java
@@ -1,12 +1,12 @@
-package com.gs;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.gs")
 public class GsApplication {
 
     public static void main(String[] args) {
+        System.out.println("sg开始启动");
         SpringApplication.run(GsApplication.class, args);
     }
 
