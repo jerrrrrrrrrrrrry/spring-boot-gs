03c0839aa4845a6f919564b1a47fa5e0d6fd969e
ShengYiqing <15900565486@163.com>
2020-04-13 01:28:09 +0800

    盛亦青更新代码

--- gs-demo/src/main/java/GsApplication.java
+++ gs-demo/src/main/java/GsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication(scanBasePackages = "com.gs")
 public class GsApplication {
 
+    //启动入口 唯一
     public static void main(String[] args) {
         System.out.println("sg开始启动");
         SpringApplication.run(GsApplication.class, args);
--- gs-demo/src/main/java/com/gs/controllers/HelloWorld.java
+++ gs-demo/src/main/java/com/gs/controllers/HelloWorld.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//apiDemo Restful风格
+
 @RestController
-@RequestMapping("demo")
+@RequestMapping("demo")//根路径
 public class HelloWorld {
 
-    @GetMapping("test")
+    @GetMapping("test")//子路径
     public String test() {
         return "Hello World!";
     }
