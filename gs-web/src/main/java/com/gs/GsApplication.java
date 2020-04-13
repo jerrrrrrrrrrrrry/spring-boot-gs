cc0ddcf2eb0c09107b04a7ee70ce05136fef16c3
Gu <gubw>
2020-04-14 02:20:59 +0800

    ver0.0.4 整合mybatis和druid

--- gs-web/src/main/java/com/gs/GsApplication.java
+++ gs-web/src/main/java/com/gs/GsApplication.java
@@ -1,9 +1,13 @@
 package com.gs;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "com.gs")
+@SpringBootApplication(scanBasePackages = "com.gs",exclude = { DataSourceAutoConfiguration.class })
+@MapperScan("com.gs.mapper")
 public class GsApplication {
 
     //启动入口 唯一
