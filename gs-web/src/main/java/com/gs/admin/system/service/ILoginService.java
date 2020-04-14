2904c10a446fadf8d023ba8a963fe79697523fee
Gu <632207717@qq.com>
2020-04-14 11:03:46 +0800

    V0.0.5 新增登陆接口

---
+++ gs-web/src/main/java/com/gs/admin/system/service/ILoginService.java
@@ -0,0 +1,20 @@
+package com.gs.admin.system.service;
+
+/**
+ * @program: gs
+ * @description: 登陆接口
+ * @author: Gu
+ * @create: 2020-04-14 10:36
+ **/
+public interface ILoginService {
+
+    /**
+    * @Description: 登陆函数
+    * @Param:  JSON格式的参数
+    * @return:  JSON格式的string字符串
+    * @Author: Gu
+    * @Date: 2020-4-14
+    */
+    String Login(String parms);
+
+}
---
+++ gs-web/src/main/java/com/gs/admin/system/service/impl/LoginServiceImpl.java
@@ -0,0 +1,16 @@
+package com.gs.admin.system.service.impl;
+
+import com.gs.admin.system.service.ILoginService;
+
+/**
+ * @program: gs
+ * @description:
+ * @author: Gu
+ * @create: 2020-04-14 10:42
+ **/
+public class LoginServiceImpl implements ILoginService {
+    @Override
+    public String Login(String parms) {
+        return null;
+    }
+}
